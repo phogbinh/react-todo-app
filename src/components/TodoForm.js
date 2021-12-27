c28def5279d8206224e7f9859107393ed68cd7f9
bill-pi4 <phongbinh2511@gmail.com>
2021-12-27 15:31:58 +0800

    [master] Changed text of update form.

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect, useRef} from 'react'
 
 function TodoForm(props) {
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState(props.edit ? props.edit.value : '')
   const inputRef = useRef(null)
   useEffect(() => {
     inputRef.current.focus()
@@ -19,16 +19,31 @@ function TodoForm(props) {
   }
   return (
     <form className='todo-form' onSubmit={handleSubmit}>
-      <input
-        type='text'
-        placeholder='Add a todo'
-        value={input}
-        name='text'
-        className='todo-input'
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      <button className='todo-button'>Add todo</button>
+      {props.edit ?
+        (<>
+        <input
+          type='text'
+          placeholder='Update your item'
+          value={input}
+          name='text'
+          className='todo-input'
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <button className='todo-button'>Update</button>
+        </>) :
+        (<>
+        <input
+          type='text'
+          placeholder='Add a todo'
+          value={input}
+          name='text'
+          className='todo-input'
+          onChange={handleChange}
+          ref={inputRef}
+        />
+        <button className='todo-button'>Add todo</button>
+        </>)}
     </form>
   )
 }
